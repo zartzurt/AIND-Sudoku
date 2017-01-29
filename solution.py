d2d4cd27e4e8dc482ce29edfb645fc6c5d4b5d06
Osman A <osman.alpay@gmail.com>
2017-01-29 22:52:43 +0300

    utils functions are put on an other file

--- solution.py
+++ solution.py
@@ -5,28 +5,7 @@ logging.basicConfig(level=logging.ERROR)
 
 assignments = []
 
-"""
-Utils
-"""
-rows = 'ABCDEFGHI'
-cols = '123456789'
-
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    return [a+b for a in A for b in B]
-
-boxes = cross(rows, cols)
-row_units = [cross(r, cols) for r in rows]
-column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI')
-                for cs in ('123', '456', '789')]
-# add diagonal units to the list of other units
-diagonal_units = [
-    ['%s%s' % t for t in zip(rows, cols)], ['%s%s' % t for t in zip(rows[::-1], cols)]]
-unitlist = row_units + column_units + square_units + diagonal_units
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
+from utils import *
 
 
 def assign_value(values, box, value):
